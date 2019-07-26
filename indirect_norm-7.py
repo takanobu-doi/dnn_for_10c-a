25035eef1a0153b4c2752ef52426187b45f1cd1a
DOI Takanobu <kumanotakanobu@outlook.jp>
2019-07-26 21:19:38 +0900

    vgg-16 like

--- indirect_norm-7.py
+++ indirect_norm-7.py
@@ -2,11 +2,14 @@ import os
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
 import numpy as np
 import time
 import keras
 from keras.models import Model
-from keras.layers import Input,Conv2D,MaxPooling2D
+from keras.layers import Input,Conv2D,MaxPooling2D,BatchNormalization
 from keras.layers import Flatten,Dense,Dropout,concatenate
 from keras.callbacks import CSVLogger
 import keras.backend as K
@@ -37,12 +40,15 @@ path = "./"
 
 cell = np.load(path+"sca-0_single_tot.npy")
 point = np.loadtxt(path+"sca-0_single_teachervalue.dat")[:,3:]
+cell = np.append(cell,np.load(path+"sca-0_more_tot.npy"),axis=0)
+for i in range(4):
+    point = np.append(point,np.loadtxt(path+"sca-0_more_"+str(i)+"_teachervalue.dat")[:,3:],axis=0)
 print(cell.shape)
-shape = cell[0][0:1].shape
-cell_test = cell[2700:]
-point_test = point[2700:]
-cell = cell[:2700]
-point = point[:2700]
+shape = cell[0].shape
+cell_test = cell[4500:]
+point_test = point[4500:]
+cell = cell[:4500]
+point = point[:4500]
 
 print(shape)
 
@@ -53,82 +59,64 @@ session = tf.Session(config=session_config)
 KTF.set_session(session)
 KTF.set_learning_phase(1)
 
-Input_a = Input(shape=shape)
-Input_c = Input(shape=shape)
-x = MaxPooling2D(pool_size=(2,2),data_format="channels_first")(Input_a)
-y = MaxPooling2D(pool_size=(2,2),data_format="channels_first")(Input_c)
-x = Conv2D(filters=40,kernel_size=16,padding="same",
+Input = Input(shape=shape)
+x = Conv2D(filters=3,kernel_size=16,padding="same",
+           activation="relu",data_format="channels_first")(Input)
+x = Conv2D(filters=3,kernel_size=16,padding="same",
            activation="relu",data_format="channels_first")(x)
-y = Conv2D(filters=40,kernel_size=16,padding="same",
-           activation="relu",data_format="channels_first")(y)
-x = Conv2D(filters=40,kernel_size=16,padding="same",
+x = MaxPooling2D(pool_size=(4,4),data_format="channels_first")(x)
+x = Conv2D(filters=3,kernel_size=32,padding="same",
            activation="relu",data_format="channels_first")(x)
-y = Conv2D(filters=40,kernel_size=16,padding="same",
-           activation="relu",data_format="channels_first")(y)
-x = Conv2D(filters=40,kernel_size=5,padding="same",
+x = Conv2D(filters=3,kernel_size=32,padding="same",
            activation="relu",data_format="channels_first")(x)
-y = Conv2D(filters=40,kernel_size=5,padding="same",
-           activation="relu",data_format="channels_first")(y)
+x = BatchNormalization(axis=1)(x)
 x = MaxPooling2D(pool_size=(2,2),data_format="channels_first")(x)
-y = MaxPooling2D(pool_size=(2,2),data_format="channels_first")(y)
-x = Conv2D(filters=40,kernel_size=8,padding="same",
+x = Conv2D(filters=3,kernel_size=64,padding="same",
            activation="relu",data_format="channels_first")(x)
-y = Conv2D(filters=40,kernel_size=8,padding="same",
-           activation="relu",data_format="channels_first")(y)
-x = Conv2D(filters=40,kernel_size=8,padding="same",
+x = Conv2D(filters=3,kernel_size=64,padding="same",
            activation="relu",data_format="channels_first")(x)
-y = Conv2D(filters=40,kernel_size=8,padding="same",
-           activation="relu",data_format="channels_first")(y)
-x = Conv2D(filters=40,kernel_size=2,padding="same",
-           activation="relu",data_format="channels_first")(x)
-y = Conv2D(filters=40,kernel_size=2,padding="same",
-           activation="relu",data_format="channels_first")(y)
+x = BatchNormalization(axis=1)(x)
 x = MaxPooling2D(pool_size=(2,2),data_format="channels_first")(x)
-y = MaxPooling2D(pool_size=(2,2),data_format="channels_first")(y)
-x = Conv2D(filters=40,kernel_size=4,padding="same",
+x = Conv2D(filters=3,kernel_size=128,padding="same",
            activation="relu",data_format="channels_first")(x)
-y = Conv2D(filters=40,kernel_size=4,padding="same",
-           activation="relu",data_format="channels_first")(y)
-x = Conv2D(filters=40,kernel_size=4,padding="same",
+x = Conv2D(filters=3,kernel_size=128,padding="same",
            activation="relu",data_format="channels_first")(x)
-y = Conv2D(filters=40,kernel_size=4,padding="same",
-           activation="relu",data_format="channels_first")(y)
-x = Conv2D(filters=40,kernel_size=1,padding="same",
+x = Conv2D(filters=3,kernel_size=128,padding="same",
            activation="relu",data_format="channels_first")(x)
-y = Conv2D(filters=40,kernel_size=1,padding="same",
-           activation="relu",data_format="channels_first")(y)
+x = BatchNormalization(axis=1)(x)
 x = MaxPooling2D(pool_size=(2,2),data_format="channels_first")(x)
-y = MaxPooling2D(pool_size=(2,2),data_format="channels_first")(y)
+x = Conv2D(filters=3,kernel_size=128,padding="same",
+           activation="relu",data_format="channels_first")(x)
+x = Conv2D(filters=3,kernel_size=128,padding="same",
+           activation="relu",data_format="channels_first")(x)
+x = Conv2D(filters=3,kernel_size=128,padding="same",
+           activation="relu",data_format="channels_first")(x)
+x = BatchNormalization(axis=1)(x)
+x = MaxPooling2D(pool_size=(4,4),data_format="channels_first")(x)
 x = Flatten()(x)
-y = Flatten()(y)
-x = Dense(128,activation="sigmoid")(x)
-y = Dense(128,activation="sigmoid")(y)
-x = Dropout(0.3)(x)
-y = Dropout(0.3)(y)
+x = Dense(2048,activation="sigmoid")(x)
+x = Dropout(0.5)(x)
 x = Dense(16,activation="sigmoid")(x)
-y = Dense(16,activation="sigmoid")(y)
-x = Dropout(0.3)(x)
-y = Dropout(0,3)(y)
-z = concatenate([x,y])
-Output = Dense(8,activation="relu")(z)
+x = Dropout(0.5)(x)
+Output = Dense(8,activation="relu")(x)
 
-model = Model(inputs=[Input_a,Input_c],outputs=Output)
-model.compile(loss="mse",optimizer="adadelta",metrics=[rms_pred_scat,rms_pred_stop])
+model = Model(inputs=Input,outputs=Output)
+model.compile(loss="mse",optimizer="adam",metrics=[rms_pred_scat,rms_pred_stop])
 csvlogger = CSVLogger("indirect_norm-7.csv")
 
-factor = [256.,1024.,256.,1024.,256.,1024.,256.,1024.]
+factor = [256,1024,256,1024,256,1024,256,1024]
 factor = np.array(factor)
 
 start = time.time()
-model.fit([cell[:,0:1],cell[:,1:2]],point/factor,epochs=100,batch_size=64,
-          validation_data=[[cell_test[:,0:1],cell_test[:,1:2]],point_test/factor],
+model.fit(cell,point/factor,epochs=100,batch_size=64,
+          validation_data=[cell_test,point_test/factor],
           callbacks=[csvlogger])
 end = time.time()
 print("Learning time is {} second".format(end-start))
 model.summary()
 model.save("indirect_norm-7.h5",{"rms_pred_scat":rms_pred_scat,"rms_pred_stop":rms_pred_stop})
 
-pred = model.predict([cell_test[:,0:1],cell_test[:,1:2]])
+pred = model.predict(cell_test)
 np.savetxt("indirect_norm-7.dat",pred*factor,header="avs avc aes aec cvs cvc ces cec [pixel]")
 
 KTF.set_session(old_session)
